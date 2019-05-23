d4aa5b4c571667b3dbb98383f6ff98f5154430b7
Morgan Peterson <47166767+MorganPeterson1313@users.noreply.github.com>
2019-05-22 23:01:41 -0400

    added a yarn build project

--- starwars/src/components/StarWars.js
+++ starwars/src/components/StarWars.js
@@ -9,8 +9,8 @@ function StarWars(props){
               <h1>{props.character.name}</h1>
               <ul>
               <li><strong>Mass: </strong> {props.character.mass}</li>
-              <li><strong>Gender: </strong>{props.character.gender}</li>
-              <li><strong>Skin Color: </strong>{props.character.skin_color}</li>
+              <li><strong>Gender:</strong>{props.character.gender}</li>
+              <li><strong>Skin Color:</strong>{props.character.skin_color}</li>
               </ul>       
       </div> 
     );
